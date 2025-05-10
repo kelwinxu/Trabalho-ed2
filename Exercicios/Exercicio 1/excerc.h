fd0e25f81cb95d08e8d86d27e5bf3e3e50149b35
Santiago06118 <boichii9090@gmail.com>
2025-05-10 12:16:36 -0300

    Update excerc.h

--- Exercicios/Exercicio 1/excerc.h
+++ Exercicios/Exercicio 1/excerc.h
@@ -1,6 +1,16 @@
 #ifndef EXCERC_H_INCLUDED
 #define EXCERC_H_INCLUDED
+#define MAX_SIZE 6
 
-void funcao_anagramas(int v[6],int tam);
+/// algoritmo "Heap's Algorithm"
+void funcao_anagramas(int v[MAX_SIZE],int tam,int **permutations);
 
+/// funcao escanear
+void escanear(int *v,int tam);
+
+/// funcao printar
+void printar(int  *v,int tam);
+
+/// aloca, dentro de um pointeiro de ponteiro, os ponteiros de tamanho MAX_SIZE.
+void **vetor_alocate(int **permutations, int tam);
 #endif // EXCERC_H_INCLUDED
