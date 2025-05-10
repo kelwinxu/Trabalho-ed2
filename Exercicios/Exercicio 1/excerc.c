3633da7fc5b3bb47088786e58bf6745bb9bb0af5
Santiago06118 <boichii9090@gmail.com>
2025-05-10 18:21:02 -0300

    Update excerc.c

--- Exercicios/Exercicio 1/excerc.c
+++ Exercicios/Exercicio 1/excerc.c
@@ -14,13 +14,12 @@ void printar(int *v,int tam){
 }
 /// obs: escaneia o vetor invertido, ou seja se 1,2,3,4,5,6 escaneia 6,5,4,3,2,1 não havera diferença nesse exercicio
 void escanear(int *v,int tam){
-    tam= tam-1;
+    tam = tam-1;
     if(tam <0){
         return;
     }
-
-    escanear(v,tam);
     scanf("%d",&v[tam]);
+    escanear(v,tam);
 }
 /// aloca, dentro de um pointeiro de ponteiro, os ponteiros de tamanho MAX_SIZE.
 void **vetor_alocate(int **permutations, int tam){
@@ -58,49 +57,87 @@ void funcao_anagramas(int v[MAX_SIZE],int size,int **permutations){
     }
     }
 }
+void F_OP(int op){
+    switch (op){
+    case 1:
+    printf("/");
+    break;
+    case 2:
+    printf("*");
+    break;
+    case 3:
+    printf("+");
+    break;
+    case 4:
+    printf("-");
+    break;
+    case 0:
+    break;
+    }
+}
+void F_ELEM(int a){
+    if(a>0){
+        printf(" %d ",a);
+    }
+}
+void F_print(int v[6],int sinais[6]){
+    for(int i=0;i<MAX_SIZE;i++){
+        F_ELEM(v[i]);
+        F_OP(sinais[i]);
+    }
+    printf("\n\n\n");
+    for(int i=0;i<MAX_SIZE;i++){
+        printf(" %d",v[i]);
+    }
+    printf("\n\n\n");
+    for(int i=0;i<MAX_SIZE;i++){
+        printf(" %d",sinais[i]);
+    }
+}
+
 void check_operation(int **permutations, int iteracao, int valor){
-
-    int *elem = (int*)malloc(MAX_SIZE*sizeof(int));
-
+    int comp;
+    int *v = (int*)malloc(MAX_SIZE*sizeof(int));
+    int *sinais = (int*)calloc(6,sizeof(int));
     if(iteracao<1){
         printf("--Nao existe solucao--");
         return;
 
     }
-    elem = permutations[iteracao-1];
+    v = permutations[iteracao-1];
     /*printar(elem,6);*/
-    int comp;
-    comp = elem[0];
+    comp = v[0];
+    comp = comp + v[1];
+    if(v[1]>0){sinais[0]=3;}
+    comp = comp - v[2];
+    if(v[2]>0){sinais[1]=4;}
 
-    if(elem[1]>0){
-            if(comp%(elem[1])==0){
-                comp = comp/(elem[1]);
-                    if(elem[2]>0){comp = comp*(elem[2]);}
-
-            }
-
-
-    }
-
-    comp = comp + elem[3];
-    comp = comp - elem[4];
-
-    if(elem[5]>0){
-        if(comp*elem[5] == valor){
-                printf("((%d / %d)* %d + %d - %d)* %d = %d ",elem[0],elem[1],elem[2],elem[3],elem[4],elem[5],valor);
-                return;
-            }
-        if(comp%elem[5]==0){
-            if(comp/elem[5] == valor ){
-                printf("((%d / %d)* %d + %d - %d)/ %d = %d ",elem[0],elem[1],elem[2],elem[3],elem[4],elem[5],valor);
-                return;
-            }
+    if(comp>0 && v[3]>0){
+        if(comp%v[3]== 0){
+            comp = (comp/(v[3]));
+            sinais[2]=1;
         }
     }
-    if((comp - elem[5]) == valor){
-        printf("((%d / %d)* %d + %d - %d) - %d = %d ",elem[0],elem[1],elem[2],elem[3],elem[4],elem[5],valor);
+    if(comp>0 && v[4]>0){
+        comp = (comp*(v[4]));
+        sinais[3]=2;
+    }
+
+    if(v[5]>0){
+        if(comp>0 && (comp+v[5])==valor){comp = comp + v[5];sinais[4]=3;}
+        if(comp>0 && (comp-v[5])==valor){comp = comp - v[5];sinais[4]=4;}
+        if(comp>0 && (comp%v[5])==0 && (comp/v[5])== valor){comp = (comp/v[5]);sinais[4]=1;}
+        if(comp>0 && (comp*v[5])== valor){comp = (comp*v[5]);sinais[4]=2;}
+    }
+    if(comp == valor){
+        printf("%d",comp);
+        F_print(permutations[iteracao-1],sinais);
         return;
     }
+
+    check_operation(permutations,iteracao-1,valor);
+
+}
 
     if((comp + elem[5]) == valor){
         printf("((%d / %d )* %d + %d - %d)  + %d = %d ",elem[0],elem[1],elem[2],elem[3],elem[4],elem[5],valor);
