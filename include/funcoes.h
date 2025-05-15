2d24e406dbbb5100f7f82a41e36c0e85dccc31c7
Felix-Gabriel-1 <felixgabriel@usp.br>
2025-05-15 11:40:20 -0300

    Delete include directory

--- include/funcoes.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCOES_H_INCLUDED
-#define FUNCOES_H_INCLUDED
-int ver_decrescente(int lista[], int esquerda, int direita);
-int encontrar_primeira_ocorrencia(int lista[], int esquerda, int direita, int valor, int decrescente);
-void buscar_valores_repetidos(int lista[], int tamanho, int valores[], int qtd_valores, int resultado[][2]);
-#endif // FUNCOES_H_INCLUDED
