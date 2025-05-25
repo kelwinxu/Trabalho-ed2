7d85cf4b6b7223b209aa2b154662c34629e7e508
Santiago06118 <boichii9090@gmail.com>
2025-05-24 23:14:55 -0300

    Update exerc3.h

--- Exercicios/Exercicio 3/exerc3.h
+++ Exercicios/Exercicio 3/exerc3.h
@@ -1,21 +1,41 @@
 #ifndef EXERC3_H_INCLUDED
 #define EXERC3_H_INCLUDED
+#define MAX_SIZE 1000
 /// estrutura da tabela
 typedef struct formulario* tabela;
 typedef struct formulario aluno;
 
-///criar arquivo txt tabela hash(hash.txt)
+////criar arquivo txt tabela hash(hash.txt)
 void *f_criararq(char *path);
-///carrega arquivo ntxt na memoria  tabela hash(hash.txt)
+////qcarrega arquivo ntxt na memoria  tabela hash(hash.txt)
 void *f_carregaarq(char *path);
-///criar tabela hash
-tabela *f_criatabela(char tam[100]);
-/// carrega a tabela na memoria
-tabela *f_carregatabela(FILE *arq,tabela *Hash);
-///insere na tabela hash que esta na memmoria
-void f_ineserenatabela(tabela *Hash, char str[100],char tam[100]);
-///insere tabela hash no arquivo txt
-void f_inserehashnorquivo(tabela *Hash,FILE *arq, char *tam);
-/// menu
-void menu(tabela *Hash, FILE *arq);
+////retorna o numero primo mais proximo
+int f_numeros_primos(int n);
+////funcao de espalhamento quadratico
+int f_espalhmamentoquadratico(tabela *Hash,int chave,int tamhash);
+////funcao de espalhamento duplo
+int f_espalhamentoduplo(tabela *Hash,int chave, int tamhash);
+//// funcao de re espalhamneto da tabela
+tabela *f_respalhamento(tabela *Hash, int *tamhash);
+////funcao XOR
+void *XOR(int **vbin,int maxdig);
+//// printa tabela hash
+void f_printa_hash(tabela *Hash, int tamhash);
+////criar tabela hash
+tabela *f_criatabela(int tamanho);
+//// carrega a tabela na memoria
+tabela *f_carregatabela(char path[100]);
+////insere na tabela hash que esta na memmoria
+tabela *f_ineserenatabela(char str[100]);
+////insere tabela hash no arquivo txt
+void f_inserehashnorquivo(tabela *Hash,char path[100]);
+//// funcao hash
+int f_chave(char nusp[MAX_SIZE],int mod);
+//// busca na tabela hash
+void f_buscahash(tabela Hash,char input[MAX_SIZE]);
+////remove um elemento da tabela
+void f_removehash(tabela *Hash,char input[MAX_SIZE]);
+//// menu
+void menu(tabela *Hash, FILE *arq, char str[100]);
 #endif // EXERC3_H_INCLUDED
+
