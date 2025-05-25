8ab87158d7e0ff5c4054c39b123e17df6847dd87
Santiago06118 <boichii9090@gmail.com>
2025-05-25 16:40:15 -0300

    Update exerc3.h

--- Exercicios/Exercicio 3/exerc3.h
+++ Exercicios/Exercicio 3/exerc3.h
@@ -5,37 +5,21 @@
 typedef struct formulario* tabela;
 typedef struct formulario aluno;
 
-////criar arquivo txt tabela hash(hash.txt)
-void *f_criararq(char *path);
-////qcarrega arquivo ntxt na memoria  tabela hash(hash.txt)
-void *f_carregaarq(char *path);
-////retorna o numero primo mais proximo
-int f_numeros_primos(int n);
-////funcao de espalhamento quadratico
-int f_espalhmamentoquadratico(tabela *Hash,int chave,int tamhash);
-////funcao de espalhamento duplo
-int f_espalhamentoduplo(tabela *Hash,int chave, int tamhash);
-//// funcao de re espalhamneto da tabela
-tabela *f_respalhamento(tabela *Hash, int *tamhash);
-////funcao XOR
-void *XOR(int **vbin,int maxdig);
-//// printa tabela hash
-void f_printa_hash(tabela *Hash, int tamhash);
-////criar tabela hash
-tabela *f_criatabela(int tamanho);
-//// carrega a tabela na memoria
-tabela *f_carregatabela(char path[100]);
-////insere na tabela hash que esta na memmoria
-tabela *f_ineserenatabela(char str[100]);
-////insere tabela hash no arquivo txt
-void f_inserehashnorquivo(tabela *Hash,char path[100]);
-//// funcao hash
-int f_chave(char nusp[MAX_SIZE],int mod);
-//// busca na tabela hash
-void f_buscahash(tabela Hash,char input[MAX_SIZE]);
-////remove um elemento da tabela
-void f_removehash(tabela *Hash,char input[MAX_SIZE]);
-//// menu
-void menu(tabela *Hash, FILE *arq, char str[100]);
-#endif // EXERC3_H_INCLUDED
 
+void *f_criararq(char *path);       ////criar arquivo txt tabela hash(hash.txt)
+void *f_carregaarq(char *path);     ////qcarrega arquivo ntxt na memoria  tabela hash(hash.txt)
+int f_numeros_primos(int n);        ////retorna o numero primo mais proximo
+int f_espalhmamentoquadratico(tabela *Hash,unsigned int chave,unsigned int tamhash);  ////funcao de espalhamento quadratico
+int f_espalhamentoduplo(tabela *Hash,unsigned int chave,unsigned int tamhash);       ////funcao de espalhamento duplo
+tabela *f_respalhamento(tabela *Hash, int *tamhash);                //// funcao de re espalhamneto da tabela
+void *XOR(int **vbin,int maxdig);                                   ////funcao XOR
+void f_printa_hash(tabela *Hash, int tamhash);                      //// printa tabela hash
+tabela *f_criatabela(int tamanho);                                  ////criar tabela hash
+tabela *f_carregatabela(char path[100]);                            //// carrega a tabela na memoria
+tabela *f_ineserenatabela(char str[100]);                           ////insere na tabela hash que esta na memmoria
+void f_inserehashnorquivo(tabela *Hash,char path[100]);             ////insere tabela hash no arquivo txt
+int f_chave(char nusp[MAX_SIZE],int mod);                           //// funcao hash
+void f_buscahash(tabela *Hash,char input[MAX_SIZE]);                //// busca na tabela hash
+void f_removehash(tabela *Hash,char input[MAX_SIZE]);               ////remove um elemento da tabela
+void menu(tabela *Hash, FILE *arq, char str[100]);                  //// menu
+#endif // EXERC3_H_INCLUDED
