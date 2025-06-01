a39795bb4ea2078a9ea0cc1a6eef48f80787c1ad
Felix-Gabriel-1 <felixgabriel@usp.br>
2025-05-31 22:20:40 -0300

    slk num compensa

---
+++ Exercicios/Exercicio 7/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_N 20
+#define MAX_SOLUCOES 100000
+
+int chamadas_recursivas = 0;
+int n; // Número de rainhas
+
+typedef struct {
+    int posicoes[MAX_N]; // índice = coluna, valor = linha
+} Solucao;
+
+Solucao solucoes[MAX_SOLUCOES];
+int total_solucoes = 0;
+
+/// Marcações para linhas e diagonais
+bool linha_ocupada[MAX_N];
+bool diagonal_principal_ocupada[2 * MAX_N];
+bool diagonal_secundaria_ocupada[2 * MAX_N];
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
