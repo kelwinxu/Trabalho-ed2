9a7379334c55681cf1ae0d3dccce35366c85fa57
Santiago06118 <boichii9090@gmail.com>
2025-06-05 11:30:14 -0300

    Update exerc4.h

--- Exercicios/Exercicio 4/exerc4.h
+++ Exercicios/Exercicio 4/exerc4.h
@@ -1,34 +1,25 @@
 #ifndef EXERC4_H_INCLUDED
 #define EXERC4_H_INCLUDED
 #define MAX_SIZE 1000
+#define MAX_INPUT 1000
 /// estrutura da tabela
 typedef struct formulario* tabela;
 typedef struct formulario aluno;
 
-////criar arquivo txt tabela hash(hash.txt)
-void *f_criararq(char *path);
-////qcarrega arquivo ntxt na memoria  tabela hash(hash.txt)
-void *f_carregaarq(char *path);
-////retorna o numero primo mais proximo
-int f_numeros_primos(int n);
-////funcao XOR
-void *XOR(int **vbin,int maxdig);
-//// printa tabela hash
-void f_printa_hash(tabela *Hash, int tamhash);
-////criar tabela hash
-tabela *f_criatabela(int tamanho);
-//// carrega a tabela na memoria
-tabela *f_carregatabela(char path[100]);
-////insere na tabela hash que esta na memmoria
-tabela *f_ineserenatabela(char str[100]);
-////insere tabela hash no arquivo txt
-void f_inserehashnorquivo(tabela *Hash,char path[100]);
-//// funcao hash
-int f_chave(char nusp[MAX_SIZE],int mod);
-//// busca na tabela hash
-void f_buscahash(tabela *Hash,char input[MAX_SIZE]);
-////remove um elemento da tabela
-void f_removehash(tabela *Hash,char input[MAX_SIZE]);
+
+void *f_criararq(char *path);                       ////criar arquivo txt tabela hash(hash.txt)
+void *f_carregaarq(char *path);                     ////qcarrega arquivo ntxt na memoria  tabela hash(hash.txt)
+int f_numeros_primos(int n);                        ////retorna o numero primo mais proximo
+void *XOR(int **vbin,int maxdig);                   ////funcao XOR
+void f_printa_hash(tabela *Hash, int tamhash);      //// printa tabela hash
+tabela *f_criatabela(int tamanho);                  ////criar tabela hash
+tabela *f_carregatabela(char *path);                //// carrega a tabela na memoria
+tabela *f_ineserenatabela(char str[MAX_INPUT]);     ////insere na tabela hash que esta na memmoria
+void f_inserehashnorquivo(tabela *Hash,char *path); ////insere tabela hash no arquivo txt
+int f_chave(char nusp[MAX_SIZE],int mod);           //// funcao hash
+void f_buscahash(char input[MAX_SIZE]);             //// busca na tabela hash
+void f_removehash(char input[MAX_SIZE]);            ////remove um elemento da tabela
 //// menu
-void menu(tabela *Hash, FILE *arq, char str[100]);
+void menu(tabela *Hash, FILE *arq, char str[MAX_INPUT]);
+
 #endif // EXERC4_H_INCLUDED
