488281d67e66292ce111713ae453a34b7db21c15
Santiago06118 <boichii9090@gmail.com>
2025-06-05 11:51:56 -0300

    Update main.c

--- Exercicios/Exercicio 3/main.c
+++ Exercicios/Exercicio 3/main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 #include "exerc3.h"
 int main(){
     tabela *hash = NULL;
     FILE *arq;
-    char str[100], comp[100], *strsave,*token, *token2;
+    char str[MAX_INPUT], comp[MAX_INPUT], *strsave,*token, *token2;
     int invalido=0;
     printf("------ excercicio3: implementacao de tabela hash ------\n -->criacao: main criar <path.txt> nusp,nome,curso,...,curso\n -->insercao: main inserir <path.txt> nusp,nome,curso,...,curso\n -->remocao:main remover <path.txt> nusp,nusp2,...,nuspn\n -->busca: main buscar <path.txt> nusp1,nusp2,..,nuspn\n ?->caracteristicas:funcao hash,espalhamento quadratico,espalhamento duplo,reespalhamento de tabela\n !->Obs:e possivel o conflito com antivirus, devido a manipulacao de arquivos\n");
-    fgets(str,100,stdin);
-    memcpy(comp,str,100);
+    fgets(str,MAX_INPUT,stdin);
+    strcpy(comp,str);
     token = NULL;
     token2 = NULL;
     token = strtok_r(comp," ",&strsave);
@@ -27,18 +26,18 @@ int main(){
     }else{invalido = 1;}
     if(invalido == 1){////se o input for invalido
         printf("----- Comando invalido, iniciando caso de teste predefinido... -----\n\n");
-        char str64[] = {"main criar 10000 hash.txt\n"};
-        char str128[] = {"main inserir hash.txt 155234831,kelwin zhao xu,engcomp,13314621,joel dutras,bcc,14323324,felipe martinez,arquitetura,12244534,gabriel dias,bci,15533467,gabriela amanda,medicina\n"};
-        char str256[] = {"main remover hash.txt 155234831,13314621\n"};
+        char str64[MAX_INPUT] = {"main criar 11 hash.txt\n"};
+        char str128[MAX_INPUT] = {"main inserir hash.txt 155234831,kelwin zhao xu,engcomp,13314621,joel dutras,bcc,14323324,felipe martinez,arquitetura,12244534,gabriel dias,bci,15533467,gabriela amanda,medicina\n"};
+        char str256[MAX_INPUT] = {"main remover hash.txt 155234831,13314621\n"};
 
-        char str512[] = {"main buscar hash.txt 14323324,1003300\n"};
+        char str512[MAX_INPUT] = {"main buscar hash.txt 14323324,1003300\n"};
         printf("Input 1: %s ",str64);
         printf("\n");
         printf("Input 2: %s ",str128);
         printf("\n");
         printf("Input 3: %s ",str256);
         printf("\n");
-        printf("Input 3: %s ",str512);
+        printf("Input 4: %s ",str512);
         printf("\n");
         menu(hash,arq,str64);
         menu(hash,arq,str128);
