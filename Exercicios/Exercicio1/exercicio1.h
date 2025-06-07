4c02de7d3b058d8bb402a07debe757fbd1abae60
Joel <joel.dutra@usp.br>
2025-06-07 20:26:03 -0300

    Delete Exercicios/Exercicio1 directory

--- Exercicios/Exercicio1/exercicio1.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXERCICIO1_H_INCLUDED
-#define EXERCICIO1_H_INCLUDED
-
-void funcao(int i, int a,int b, int c,int d,int e,int valor_alvo, int k);
-int while_recursivo(char *valores, int numeros[20],int i, int *tam);
-
-#endif // EXERCICIO1_H_INCLUDED
